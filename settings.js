e36e0e33693a91d71bd1b5057392eceed97a7c9c
vudinh <dinhquangvu1@gmail.com>
2017-11-14 15:58:26 +0700

    no message

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
-  verify_token: 'y0ur_0wn_t0k3n',
+  verify_token: '1a73788c68f9d74d188f6c5fc57e28a4',
   access_token: 'EAASDZAyXUyCMBAJCrk0PVEQDZAk5qF1Rc8fuZC4ATK4gmhX3meXZCapKzJ3PVwmZCG2tD7PkFsILi29PmZCUhMnIFuLMzJWZBuK4NFfFj2eVS5i0vLgD54B3u8dMuXteXISFX0sRZAjy6Ri4sdKv53DWBAdVhxfJ09alWpWYSnhTqQZDZD',
 };
